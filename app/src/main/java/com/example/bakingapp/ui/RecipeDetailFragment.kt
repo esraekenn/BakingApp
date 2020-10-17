756021efbbe9fb6f1162fbe2b2e57a6952978839
EsraEKEN <esraekennnn@gmail.com>
2020-10-17 23:55:34 +0300

    bug fix

--- app/src/main/java/com/example/bakingapp/ui/RecipeDetailFragment.kt
+++ app/src/main/java/com/example/bakingapp/ui/RecipeDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.example.bakingapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bakingapp.R
 import com.example.bakingapp.databinding.FragmentDetailRecipeBinding
 import com.example.bakingapp.dto.BakingServiceProvider
-import com.example.bakingapp.dto.Steps
+import com.example.bakingapp.dto.Recipe
 import com.example.bakingapp.ui.adapter.RecipeDetailListAdapter
 import kotlinx.android.synthetic.main.fragment_recipe.*
 import retrofit2.Call
@@ -37,24 +38,30 @@ class RecipeDetailFragment : Fragment() {
 
     private fun renderUI() {
         val bakingServiceProvider = BakingServiceProvider()
-        bakingServiceProvider.BakingService().getSteps().enqueue(object : Callback<List<Steps>> {
-            override fun onFailure(call: Call<List<Steps>>, t: Throwable) {
+        bakingServiceProvider.BakingService().getBaking().enqueue(object : Callback<List<Recipe>> {
+            override fun onFailure(call: Call<List<Recipe>>, t: Throwable) {
+
+                Log.v("dfjf", "d")
 
             }
 
-            override fun onResponse(call: Call<List<Steps>>, response: Response<List<Steps>>) {
+            override fun onResponse(call: Call<List<Recipe>>, response: Response<List<Recipe>>) {
                 rcList.layoutManager = LinearLayoutManager(activity)
                 val bakinglist = ArrayList(response.body()!!)
                 binding.rcList.adapter =
-                    RecipeDetailListAdapter(bakinglist) { steps: Steps, position: Int ->
+                    RecipeDetailListAdapter(bakinglist) { steps: Recipe, position: Int ->
+                        val recipeDetailFragment = RecipeDetailFragment()
+                        fragmentManager!!.beginTransaction()
+                            .replace(R.id.kase, recipeDetailFragment).commit()
+                        Log.v("dfjf", "kfjslkkjsfkl")
+
 
                     }
-            }
 
+
+            }
 
         })
 
     }
-
-
 }
